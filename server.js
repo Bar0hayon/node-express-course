df175f2417ae82f87bcfbc8e5a55ff404176ab9c
BarOhayon <barohayon27@gmail.com>
2020-04-22 19:59:44 +0300

    add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
 const mockUserData=[
 	{name:'Mark'},
 	{name:'Jill'}
 ]
+
+app.post('/login', function(request, response){
+    const username = request.body.username;
+    const password = request.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if ( username === mockUsername && password === mockPassword){
+         response.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         response.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
+})
 
 app.get('/users', function(request,response){
 	response.json({
