c35d80b75b059891aef4396b125af6a9e4190659
BarOhayon <barohayon27@gmail.com>
2020-04-22 19:44:43 +0300

    add dynamic GET route

--- server.js
+++ server.js
@@ -14,4 +14,13 @@ app.get('/users', function(request,response){
 	})
 })
 
+app.get('/users/:id',function(request, response){
+	console.log(request.params.id)
+	response.json({
+		success: true,
+		message: 'got one user',
+		user: request.params.id
+	})
+})
+
 app.listen(8000,function(){console.log('server is listening')})
